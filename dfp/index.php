93cb4bce7d25733310506627f2ff849968cb49b3
Sora Su <sora.su@condenast.com.tw>
2014-12-22 13:06:32 +0800

    change page title to "DFP test"

--- dfp/index.php
+++ dfp/index.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>DFP demo</title>
+        <title>DFP test</title>
 <!-- DFP -->
 <script type='text/javascript'>
 (function() {
