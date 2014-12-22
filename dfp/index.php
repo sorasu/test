4aa46e12905dcfc9b998d3990c20219daace9a60
Sora Su <sora.su@condenast.com.tw>
2014-12-22 12:31:14 +0800

    first commit
    commit DFP test files

---
+++ dfp/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>DFP demo</title>
+<!-- DFP -->
+<script type='text/javascript'>
+(function() {
+var useSSL = 'https:' == document.location.protocol;
+var src = (useSSL ? 'https:' : 'http:') +
+'//www.googletagservices.com/tag/js/gpt.js';
+document.write('<scr' + 'ipt src="' + src + '"></scr' + 'ipt>');
+})();
+</script>
+
+<script type='text/javascript'>
+googletag.defineSlot('/25588978/gq_home_girl_r1_300x250', [300, 250], 'div-gpt-ad-1418210623948-0').addService(googletag.pubads());
+googletag.pubads().enableSyncRendering();
+googletag.pubads().enableSingleRequest();
+googletag.enableServices();
+</script>
+<!-- END DFP -->
+    </head>
+    <body>
+	<!-- DFP -->
+<!-- gq_home_girl_r1_300x250 -->
+<div class="IndexCover" id='div-gpt-ad-1418210623948-0' style='width:300px; height:250px;'>
+<script type='text/javascript'>
+googletag.display('div-gpt-ad-1418210623948-0');
+</script>
+</div>
+	<!-- END DFP -->
+    </body>
+</html>
